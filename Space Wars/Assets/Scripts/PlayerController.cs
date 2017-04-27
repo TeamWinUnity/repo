ded04ca508b2ae4392833e30ba08472a9d183954
SergeyLucash <serghei.lucas@ati.utm.md>
2017-04-27 23:09:05 +0300

    Task 1 Implemented

---
+++ Space Wars/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class Bounds
+    {
+        public float XMin, XMax;
+        public float ZMin, ZMax;
+    }
+
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody _body;
+
+        public Bounds GameBounds;
+        public float Speed;
+        public float Tilt;
+
+        void Start()
+        {
+            _body = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, 0, vertical);
+            _body.velocity = movement * Speed;
+
+            _body.position = new Vector3(
+                Mathf.Clamp(_body.position.x, GameBounds.XMin, GameBounds.XMax),
+                0,
+                Mathf.Clamp(_body.position.z, GameBounds.ZMin, GameBounds.ZMax));
+            
+            _body.rotation = Quaternion.Euler(_body.velocity.z > 0 ? _body.velocity.z * Tilt / 2 : 0, 0, _body.velocity.x * -Tilt);
+        }
+    }
+}
