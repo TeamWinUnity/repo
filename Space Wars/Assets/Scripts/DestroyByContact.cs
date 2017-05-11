e0e2ebe6c981e596718b8c03567c7039cc0c8201
radixxx <radov.2011@gmail.com>
2017-05-12 00:11:01 +0300

    added Explosion and Game Controller

--- Space Wars/Assets/Scripts/DestroyByContact.cs
+++ Space Wars/Assets/Scripts/DestroyByContact.cs
@@ -3,9 +3,18 @@
 namespace Assets.Scripts
 {
     public class DestroyByContact : MonoBehaviour {
+        public GameObject explosion;
+        public GameObject playerExplosion;
+
         void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Boundary") return;
+
+            Instantiate(explosion, transform.position, transform.rotation);
+            if (other.tag == "Player")
+            {
+                Instantiate(playerExplosion, other.transform.position, transform.rotation);
+            }
 
             Destroy(other.gameObject);
             Destroy(gameObject);
